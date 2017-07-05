927e35766940fbbef9d6e544dd4d6da17479fd3e
Ishan <ishanm6@gmail.com>
2017-07-05 13:20:38 +0530

    Assignment1.2
    Sum of two numbers.

---
+++ Assignment1.2/src/assignment1/pkg2/Assignment12.java
@@ -0,0 +1,17 @@
+
+package assignment1.pkg2;
+
+
+public class Assignment12 {
+
+   
+    public static void main(String[] args) {
+    
+       int a=2;
+       int b=3;
+       int c;
+       c=a-(-b);
+       System.out.println(""+c);
+    }
+    
+}
